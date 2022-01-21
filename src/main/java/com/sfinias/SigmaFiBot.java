d98f75734d40d91398d99cc88585e8189f893bc1
Ioannis Sfinias <ioannis.sfinias@lunatech.nl>
2022-01-21 15:48:09 +0100

    Add project buttons on SigmaFiBot

--- src/main/java/com/sfinias/SigmaFiBot.java
+++ src/main/java/com/sfinias/SigmaFiBot.java
@@ -2,9 +2,14 @@ package com.sfinias;
 
 import com.sfinias.model.CatModel;
 import com.sfinias.model.MemeModel;
+import com.sfinias.model.ProjectModel;
 import com.sfinias.resource.CatResource;
 import com.sfinias.resource.MemeResource;
+import com.sfinias.resource.TogglResource;
 import io.quarkus.logging.Log;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import javax.enterprise.context.ApplicationScoped;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
@@ -14,6 +19,8 @@ import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @ApplicationScoped
@@ -26,10 +33,13 @@ public class SigmaFiBot extends TelegramLongPollingBot {
 
     MemeResource memeResource;
 
-    public SigmaFiBot(CatResource catResource, MemeResource memeResource) {
+    TogglResource togglResource;
+
+    public SigmaFiBot(CatResource catResource, MemeResource memeResource, TogglResource togglResource) {
 
         this.catResource = catResource;
         this.memeResource = memeResource;
+        this.togglResource = togglResource;
     }
 
     @Override
@@ -67,6 +77,9 @@ public class SigmaFiBot extends TelegramLongPollingBot {
             case "/dankmeme":
                 sendDankMeme(user);
                 break;
+            case "/options":
+                sendOptions(user);
+                break;
             case "/start":
             default:
                 start(user);
@@ -81,6 +94,38 @@ public class SigmaFiBot extends TelegramLongPollingBot {
                 .build();
         try {
             execute(reply);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sendOptions(User user) {
+
+        String answer = "Choose one of the projects";
+        List<InlineKeyboardButton> projects = togglResource.getLunatechProjects().stream()
+                .map(ProjectModel::getName)
+                .map(projectName -> InlineKeyboardButton.builder().text(projectName).callbackData(projectName).build())
+                .collect(Collectors.toList());
+        List<List<InlineKeyboardButton>> buttons = new ArrayList<>();
+        List<InlineKeyboardButton> tempList = new ArrayList<>();
+        for (int i = 0; i < projects.size(); i++) {
+            InlineKeyboardButton project = projects.get(i);
+            tempList.add(project);
+            if (tempList.size() == 2 || i + 1 == projects.size()) {
+                buttons.add(tempList);
+                tempList = new ArrayList<>();
+            }
+        }
+        InlineKeyboardMarkup markupInline = InlineKeyboardMarkup.builder()
+                .keyboard(buttons)
+                .build();
+        SendMessage sendMessage = SendMessage.builder()
+                .text(answer)
+                .chatId(String.valueOf(user.getId()))
+                .replyMarkup(markupInline)
+                .build();
+        try {
+            execute(sendMessage);
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
@@ -119,6 +164,7 @@ public class SigmaFiBot extends TelegramLongPollingBot {
     }
 
     private void sendMediaResponse(User user, String url) {
+
         if (url.endsWith(".gif")) {
             sendAnimation(user, url);
         } else {
@@ -129,7 +175,7 @@ public class SigmaFiBot extends TelegramLongPollingBot {
     private void sendPhoto(User user, String url) {
 
         SendPhoto photo = SendPhoto.builder()
-                .chatId(String.valueOf(user.getId().longValue()))
+                .chatId(String.valueOf(user.getId()))
                 .photo(new InputFile(url))
                 .build();
         try {
@@ -142,7 +188,7 @@ public class SigmaFiBot extends TelegramLongPollingBot {
     private void sendAnimation(User user, String url) {
 
         SendAnimation animation = SendAnimation.builder()
-                .chatId(String.valueOf(user.getId().longValue()))
+                .chatId(String.valueOf(user.getId()))
                 .animation(new InputFile(url))
                 .build();
         try {
